29f8bfe134c0266659dbb021df0311a1893cf044
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-23 16:07:12 +0000

    C99 for loop

---
+++ C99check.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+	
+	int main( int argc[], char **argv[])
+{	
+	for (int x=1; x<5; ++x)
+	printf("C99 allows the init. part to include declaration\n");
+}
+
---
+++ functionCALL.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	void shout(void)
+	{printf("It looks like the function call works\n");}
+
+	int main(int argc[], char **argv[])
+{
+	for (int x=0; x<=5; ++x)
+	 	shout();
+}
